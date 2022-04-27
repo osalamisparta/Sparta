dd69804e92101bc2f58391eee6520b1e0d5f54c0
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-04-27 10:40:53 +0100

    Update

--- collections_intro.py
+++ collections_intro.py
@@ -3,3 +3,8 @@ print(shopping_list)
 shopping_list.append('chocolate')
 print(shopping_list)
 print(shopping_list[0])
+
+essentials_tuple = ('bread','eggs')
+print(essentials_tuple)
+print(essentials_tuple)
+
