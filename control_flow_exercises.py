11f3f324966a25135f8eec070f374bb377d522d9
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-04-27 12:38:59 +0100

    Update: Sets + Frozen Sets + Embedded List + Embedded Dictionaries

---
+++ control_flow_exercises.py
@@ -0,0 +1,121 @@
+print("\nQ1a\n")
+# Q1a: Print only the first 5 numbers in this list
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1a:
+print (x[0:5])
+
+
+
+print("\nQ1b\n")
+# Q1b: Now print only the even numbers in this list (the elements that are themselves even)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1b:
+even = []
+for i in x:
+    if i % 2 == 0:
+        even.append(i)
+print (even)
+
+
+print("\nQ1c\n")
+# Q1c: Now only print the even numbers up to the fifth element in the list (e.g. 2, 4, 34)
+x = [2, 5, 4, 87, 34, 2, 1, 31, 103, 99]
+
+# A1c:
+even_to_5 = []
+i = 0
+while i < 5:
+    if x[i] % 2 == 0:
+        even_to_5.append(x[i])
+    i += 1
+print(even_to_5)
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ2a\n")
+# Q2a: from the list of names, create another list that consists of only the first letters of each first name
+# e.g. ["Alan Turing", "Leonardo Fibonacci"] -> ["A", "L"]
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2a:
+list = []
+for letter in names:
+    list.append(letter[0])
+print(list)
+
+
+
+print("\nQ2b\n")
+# Q2b: from the list of names, create another list that consists of only the index of the space in the string
+# HINT: use your_string.index("substring")
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2b:
+list = []
+for space in names:
+    list.append(space.index(' '))
+print(list)
+
+
+
+print("\nQ2c\n")
+# Q2c: from the list of names, create another list that consists of the first and last initial of each individual
+names = ["Alan Turing", "Leonardo Fibonacci", "Katherine Johnson", "Annie Easley", "Terence Tao"]
+
+# A2c:
+list = []
+for space in names:
+    list.append(space[0]+space[space.index(' ')+1])
+print(list)
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ3a\n")
+# Q3a: Here is a list of lists, print only the lists which have no duplicates
+# Hint: This can be easily done by using sets as a set does not contain duplicates
+list_of_lists = [[1,5,7,3,44,4,1],
+                 ["A", "B", "C"],
+                 ["Hi", "Hello", "Ciao", "By", "Goodbye", "Ciao"],
+                 ["one", "Two", "Three", "Four"]]
+
+
+# A3a:
+for l in list_of_lists:
+    if len(l) == len(set(l)):
+        print (l)
+
+# -------------------------------------------------------------------------------------- #
+
+print("\nQ4a\n")
+# Q4a: Using a while loop, ask the user to input a number greater than 100, if they enter anything else,
+# get them to enter again (and repeat until the conditions are satisfied). Finally print the number that
+# they entered
+
+# A4a:
+while True:
+    num = input ('Please enter a number greater than 100. ')
+    if num.isdigit() and int(num) > 100:
+        break
+    else:
+        print('Please enter a valid number')
+num = int(num)
+print('Your number is: ', num)
+
+print("\nQ4b\n")
+# Q4b: Continue this code and print "prime" if the number is a prime number and "not prime" otherwise
+
+# A4b:
+
+count = int(2)
+while count < int(num)-1:
+    if num % count > 0:
+        count +=1
+    if num % count == 0:
+        print('Not Prime')
+        break
+
+count +=1
+if count == num:
+    print ('Prime')
