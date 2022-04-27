0a744756254e0da6b6f66352db6e0b54a1469ea2
osalamisparta <104364994+osalamisparta@users.noreply.github.com>
2022-04-27 10:46:16 +0100

    Update: Sets + Frozen Sets

--- collections_intro.py
+++ collections_intro.py
@@ -4,7 +4,16 @@ shopping_list.append('chocolate')
 print(shopping_list)
 print(shopping_list[0])
 
+#Tuple
 essentials_tuple = ('bread','eggs')
 print(essentials_tuple)
 print(essentials_tuple)
 
+
+#Sets arent ordered
+car_parts = {'wheels','doors','windows'}
+print(car_parts)
+
+#Frozen sets are immutable
+x = frozenset([1,2,3,4])
+print(x)
